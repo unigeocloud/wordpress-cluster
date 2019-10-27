e49cd807655fe4716e419930693ff1c5d99b1042
unigeocloud <unigeocloud@gmail.com>
2019-10-27 18:07:45 +0200

    Update beforeinstall.js

--- scripts/beforeinstall.js
+++ scripts/beforeinstall.js
@@ -2,15 +2,6 @@ var resp = {
   result: 0,
   ssl: !!jelastic.billing.account.GetQuotas('environment.jelasticssl.enabled').array[0].value,
   nodes: [{
-    nodeType: "storage",
-    cloudlets: 8,
-    nodeGroup: "storage",
-    displayName: "Storage"
-  }]
-}
-
-if (${settings.galera:false}) {
-  resp.nodes.push({
     nodeType: "mariadb-dockerized",
     tag: "10.3.16",
     count: 3,
@@ -22,18 +13,12 @@ if (${settings.galera:false}) {
       ON_ENV_INSTALL: "",
       JELASTIC_PORTS: "4567,4568,4444"
     }
-  })
-}
-
-if (!${settings.galera:false}) {
-  resp.nodes.push({
-    nodeType: "mariadb-dockerized",
-    tag: "10.3.16",
-    count: 1,
-    cloudlets: 16,
-    nodeGroup: "sqldb",
-    displayName: "DB Server"
-  })
+  }, {
+    nodeType: "storage",
+    cloudlets: 8,
+    nodeGroup: "storage",
+    displayName: "Storage"
+  }]
 }
 
 if (${settings.ls-addon:false}) {
@@ -55,8 +40,7 @@ if (${settings.ls-addon:false}) {
     displayName: "AppServer",
     env: {
       SERVER_WEBROOT: "/var/www/webroot/ROOT",
-      REDIS_ENABLED: "true",
-      WAF: "${settings.waf}"
+      REDIS_ENABLED: "true"
     },
     volumes: [
       "/var/www/webroot/ROOT",
@@ -70,7 +54,7 @@ if (${settings.ls-addon:false}) {
       },   
       "/var/www/webroot/.cache": {
         readOnly: "false",
-        sourcePath: "/data/.cache",
+        sourcePath: "/data/cache",
         sourceNodeGroup: "storage"
       }
     }
@@ -89,7 +73,7 @@ if (!${settings.ls-addon:false}) {
   }, {
     nodeType: "nginxphp-dockerized",
     tag: "1.16.0-php-7.3.8",
-    count: 1,
+    count: 2,
     cloudlets: 8,
     nodeGroup: "cp",
     scalingMode: "STATELESS",
@@ -100,12 +84,18 @@ if (!${settings.ls-addon:false}) {
     },
     volumes: [
       "/var/www/webroot/ROOT",
+      "/var/www/webroot/.cache",
       "/etc/nginx/conf.d/SITES_ENABLED"
     ],  
     volumeMounts: {
       "/var/www/webroot/ROOT": {
         readOnly: "false",
         sourcePath: "/data/ROOT",
+        sourceNodeGroup: "storage"
+      },
+      "/var/www/webroot/.cache": {
+        readOnly: "false",
+        sourcePath: "/data/cache",
         sourceNodeGroup: "storage"
       },
       "/etc/nginx/conf.d/SITES_ENABLED": {
